73eadbea25ef8d35e79afab4a6898056c48bd74a
Hunter Codexs <huntercodexs@gmail.com>
2025-04-24 21:43:53 -0300

    feat: [stable] Adding handler exception for HTTP Requests - Refactor

--- src/main/java/com/webflux/sample/handler/reactor/GlobalExceptionHandlerReactor.java
+++ src/main/java/com/webflux/sample/handler/reactor/GlobalExceptionHandlerReactor.java
@@ -7,39 +7,44 @@ import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
 import org.springframework.core.annotation.Order;
 import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.core.io.buffer.DataBufferFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-import java.nio.charset.StandardCharsets;
-
 @Order(-2)
 @Component
 public class GlobalExceptionHandlerReactor implements ErrorWebExceptionHandler {
 
-    public Mono<Void> handle(ServerWebExchange exchange, Throwable throwable) {
+    public Mono<Void> handle(ServerWebExchange webExchange, Throwable throwable) {
 
         if (throwable instanceof BadRequestExceptionReactor ex) {
-
-            exchange.getResponse().getHeaders().setContentType(MediaType.APPLICATION_JSON);
-            exchange.getResponse().setStatusCode(ex.getStatus());
-
-            ObjectMapper mapper = new ObjectMapper();
-            String response = null;
-            try {
-                response = mapper.writeValueAsString(new CustomResponseExceptionReactor(ex.getMessage(), ex.getCode(), ex.getTrack()));
-            } catch (JsonProcessingException e) {
-                exchange.getResponse().setStatusCode(org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR);
-                return exchange.getResponse().writeWith(Mono.just(exchange.getResponse().bufferFactory().wrap("Error converting to JSON".getBytes(StandardCharsets.UTF_8))));
-            }
-
-            DataBufferFactory bufferFactory = exchange.getResponse().bufferFactory();
-            DataBuffer dataBuffer = bufferFactory.wrap(response.getBytes());
-
-            return exchange.getResponse().writeWith(Mono.just(dataBuffer)).then();
+            ServerWebExchange exchange = this.exchange(webExchange, ex.getStatus());
+            String response = this.response(ex);
+            return exchange.getResponse().writeWith(Mono.just(buffer(exchange, response))).then();
         }
 
         throw new RuntimeException(throwable.getMessage());
+    }
+
+    private ServerWebExchange exchange(ServerWebExchange exchange, HttpStatus status) {
+        exchange.getResponse().getHeaders().setContentType(MediaType.APPLICATION_JSON);
+        exchange.getResponse().setStatusCode(status);
+        return exchange;
+    }
+
+    private String response(BadRequestExceptionReactor ex) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            return mapper.writeValueAsString(new CustomResponseExceptionReactor(ex.getMessage(), ex.getCode(), ex.getTrack()));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    private DataBuffer buffer(ServerWebExchange headers, String response) {
+        DataBufferFactory bufferFactory = headers.getResponse().bufferFactory();
+        return bufferFactory.wrap(response.getBytes());
     }
 }
